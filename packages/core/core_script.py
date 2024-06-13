2f083de0e34e541fbbca33b9ce2034c8ff198ebd
Fran <Ditarex95@gmail.com>
2024-06-13 13:18:05 +0200

    Correcciones de input precio

--- packages/core/core_script.py
+++ packages/core/core_script.py
@@ -39,10 +39,10 @@ def AddDestino():
     while True:
         precioDestino = input("Introduzca el precio del nuevo destino ---> : ")
         try:
-            precioDestino = round(float(precioDestino), 2)
+            precioDestino = round(float(precioDestino),2)
             if precioDestino <= 0:
                 print("\n<---((( ¡¡El precio tiene que ser mayor que 0!! )))--->\n")
-            elif len(precioDestino) > 5:
+            elif precioDestino > 99999.99:
                 print("\n<---((( ¡¡El precio no puede tener mas de 4 ceros!! )))--->\n")
             else:
                 break
